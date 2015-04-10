014de8f168b035a7a1ef60bd48f832b9a5706e45
Tomazh <tomaz_haidar@hotmail.com>
2015-04-10 09:50:59 -0300

    Create Ex3_medias.py

---
+++ Ex3_medias.py
@@ -0,0 +1,13 @@
+n = 0
+a = 0
+i = 0
+
+while True:
+    n = float(raw_input('Digite a nota dos alunos: '))
+    if n <0:
+        break
+    a = n + a
+    i = i + 1
+    media = a/i
+
+print "A media dos seus alunos eh " + str(media)
