0186829b741cb659420f7d0e67bd10765321cc1b
Tomazh <tomaz_haidar@hotmail.com>
2015-04-10 11:12:52 -0300

    Create Ex8.py

---
+++ Ex8.py
@@ -0,0 +1,20 @@
+notas = []
+n = 0
+a = 0
+i = 0
+
+while True:
+    try:
+        n = float(raw_input('Digite a nota dos alunos: '))
+    except:
+        print('Numero nao eh valido')
+        
+    if n <0:
+        break
+    notas.append(n)
+    a = n + a
+    i = i + 1
+    media = a/i
+
+print "\nA media dos seus alunos eh " + str(media)
+print 'A nota mais alta eh: ' + str(max(notas))
